cb5724b93de44013840a8833f88d90adf781423e
Minor <minorcordero21@gmail.com>
2021-10-06 09:18:14 -0600

    mi primer commit

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebpack    = require('html-webpack-plugin')
+const MiniCssExtract = require('mini-css-extract-plugin');
+const CopyPlugin     = require("copy-webpack-plugin"); //para copiar caperta de archivos staticos a dist
+
+module.exports = {
+    
+    mode: "development",
+
+    //sirve para lipiar la carpeta dist, por si quedaran archivos viejos
+    output: {
+        clean: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles.css$/,
+                use: [ 'style-loader', 'css-loader']
+            },
+            {
+                test: /styles.css$/,
+                use: [ MiniCssExtract.loader, 'css-loader' ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+
+    optimization: {},
+
+    plugins: [
+        new HtmlWebpack({
+            title: 'Mi Webpack App',
+            // filename: 'index.html',
+            template: './src/index.html' //el archivo que quiero que se base 
+        }),
+        
+        new MiniCssExtract({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets/', to: 'assets/' }
+            ]
+        })
+    ]
+}
+
+
